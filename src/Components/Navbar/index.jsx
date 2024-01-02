10c17da58677b8d6195e009000a7f6dc761ffff3
davdev <david.davila@correo.unimet.edu.ve>
2024-01-02 10:49:50 -0400

    NarBarItem component created. NavBar component refactored

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,72 +1,53 @@
-import { NavLink } from "react-router-dom";
+import { NavBarItem } from "../NavBarItem";
 
+const baggieLI = { pendingStyle: '' , activeStyle: '', transitioningStyle: ''}
+
+const leftUL = [
+	{ liClassName: "font-semibold text-lg" , path: '/' , text: 'Baggie' , stylesNavLink: baggieLI },
+	{ liClassName: "" , path:'/' , text: 'All' , stylesNavLink: {}},
+	{ liClassName: "" , path:'/clothes' , text: 'Clothes' , stylesNavLink: {}},
+	{ liClassName: "" , path:'/electronics' , text: 'Electronics' , stylesNavLink: {}},
+	{ liClassName: "" , path:'/furnitures' , text: 'Furnitures' , stylesNavLink: {}},
+	{ liClassName: "" , path:'/toys' , text: 'Toys' , stylesNavLink: {}},
+	{ liClassName: "" , path:'/others' , text: 'Others' , stylesNavLink: {}}
+]
+
+const rightUL = [
+	{ liClassName: "" , path:'/my-orders' , text: 'My Orders' , stylesNavLink: {}},
+	{ liClassName: "" , path:'/my-account' , text: 'My Account' , stylesNavLink: {}},
+	{ liClassName: "" , path:'/sign-in' , text: 'Sign In' , stylesNavLink: {}},
+	{ liClassName: "" , path:'/cart' , text: 'Cart 0' , stylesNavLink: {}}
+]
 
 const Navbar = () => {
-	const activeStyle = 'underline'
+	
   return (
-    <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light">
+    <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light">
       <ul className="flex items-center gap-3">
-				<li className="font-semibold text-lg">
-					<NavLink to= '/'>
-						Baggie
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to= '/'>
-						All
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to= '/clothes'>
-						Clothes
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to= '/electronics'>
-						Electronics
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to= '/furnitures'>
-						Furnitures
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to= '/toys'>
-						Toys
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to= '/others'>
-						Others
-					</NavLink>
-				</li>
+				{leftUL.map( ({liClassName, path, text, stylesNavLink}) => (
+					<NavBarItem 
+					key = {text}
+					liClassName={liClassName} 
+					path={path} 
+					text={text} 
+					stylesNavLink={stylesNavLink} />
+				)
+				)}
 			</ul>
 
       <ul className="flex items-center gap-3">
-				<li>
+				<li className="text-black/60">
 					davila.dmo@gmail.com
 				</li>
-				<li>
-					<NavLink to= '/my-orders'>
-						My Orders
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to= '/my-account'>
-						My Account
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to= '/sign-in'>
-						Sign In
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to= '/cart'>
-						Cart 0
-					</NavLink>
-				</li>
+				{rightUL.map( ({liClassName, path, text, stylesNavLink}) => (
+					<NavBarItem 
+					key = {text}
+					liClassName={liClassName} 
+					path={path} 
+					text={text} 
+					stylesNavLink={stylesNavLink} />
+				)
+				)}
 			</ul>
     </nav>
   );
