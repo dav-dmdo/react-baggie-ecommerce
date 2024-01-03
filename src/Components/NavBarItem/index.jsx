2ca21c0cc8c06e2ffc4627f687fe52f277e9bdf6
David Davila <119976378+dav-dmdo@users.noreply.github.com>
2024-01-03 14:43:08 -0400

    NarBarItem component created. NavBar component refactored (#2)
    * NarBarItem component created. NavBar component refactored
    
    * lil formatting

---
+++ src/Components/NavBarItem/index.jsx
@@ -0,0 +1,37 @@
+
+import { PropTypes } from 'prop-types';
+import { NavLink } from "react-router-dom";
+
+
+const NavBarItem = ({ liClassName, path, text, stylesNavLink }) => {
+  
+  NavBarItem.propTypes = {
+    liClassName: PropTypes.string.isRequired,
+    path: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    stylesNavLink: PropTypes.json
+  }
+
+  const {
+    pendingStyle = '' , 
+    activeStyle = 'underline underline-offset-4', 
+    transitioningStyle = ''
+  } = stylesNavLink;
+
+  const toggle =({ isActive, isPending, isTransitioning }) =>
+  [
+    isPending ? pendingStyle : "",
+    isActive ? activeStyle : "",
+    isTransitioning ? transitioningStyle : "",
+  ].join(" ")
+
+    return(
+      <li className={`${liClassName}`}>
+        <NavLink to={path} className={toggle}>
+          {text}
+        </NavLink>
+      </li>    
+    );
+}
+
+export { NavBarItem };
