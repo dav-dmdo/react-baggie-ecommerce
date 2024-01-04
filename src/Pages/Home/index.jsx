4374ec0ac08fb40ef655e8dbe0aed401dc052e93
David Davila <119976378+dav-dmdo@users.noreply.github.com>
2024-01-04 19:56:15 -0400

    Revert "Card component"

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,9 @@
 import { Layout } from "../../Components/Layout";
-import { Card } from "../../Components/Card";
+
 const Home = () => {
   return (
     <Layout>
       Home
-      <Card />
     </Layout>
     );
 };
