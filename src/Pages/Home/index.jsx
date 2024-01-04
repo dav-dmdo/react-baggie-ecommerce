fb3bbca865edf2953273358608bcf697b3c16bb7
davdev <david.davila@correo.unimet.edu.ve>
2024-01-04 19:53:54 -0400

    Card component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,10 @@
 import { Layout } from "../../Components/Layout";
-
+import { Card } from "../../Components/Card";
 const Home = () => {
   return (
     <Layout>
       Home
+      <Card />
     </Layout>
     );
 };
